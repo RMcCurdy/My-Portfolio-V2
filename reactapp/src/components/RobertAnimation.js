ee6e2268768c5faed4f00ac0c596268e830a47c3
Robert McCurdy <rwmccurdy@hotmail.com>
2021-12-27 23:00:13 -0800

    Updated Animation

--- reactapp/src/components/RobertAnimation.js
+++ reactapp/src/components/RobertAnimation.js
@@ -3,27 +3,29 @@ import robert from '../images/robert.png';
 import robert2 from '../images/robert2.png';
 import robert3 from '../images/robert3.png';
 import robert4 from '../images/robert4.png';
+import robert5 from '../images/robert5.png';
+import robert6 from '../images/robert6.png';
 
 const RobertAnimation = () => {
-    const imgArray = [robert, robert2, robert3, robert4];
+    const imgArray = [robert, robert2, robert3, robert4, robert5, robert6];
 
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            if (index === 3) {
+            if (index === 5) {
                 setIndex(0);
             } else {
                 setIndex(index + 1);
             }
-        }, 100);
+        }, 50);
         return () => clearInterval(interval);
     }, [index]);
 
     return (
         <div>
             <img
-                style={{ width: '440px' }}
+                style={{ width: '200px' }}
                 src={imgArray[index]}
                 alt='walkingRobert'
             />
