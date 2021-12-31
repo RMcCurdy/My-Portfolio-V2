c1df98626435a48eb054c0dd37ae0691c7d47338
Robert McCurdy <rwmccurdy@hotmail.com>
2021-12-30 21:27:05 -0800

    Home Page done for now

--- reactapp/src/components/HomePageMessage.js
+++ reactapp/src/components/HomePageMessage.js
@@ -1,11 +1,15 @@
 const HomePageMessage = () => {
     return (
-        <div className='home-title-container'>
-            <div className='home-title-font'>Hey, I am Robert</div>
-            <div style={{ marginTop: '0.5rem' }} className='home-title-font'>
-                University Student and
+        <div className='title-text title-text-fade-in'>
+            <div className='home-title-container'>
+                <div className='home-title-font'>Hey, I'm Robert</div>
+                <div
+                    style={{ marginTop: '0.5rem' }}
+                    className='home-title-font'>
+                    University Student and
+                </div>
+                <div className='home-title-font'> aspiring Web Developer</div>
             </div>
-            <div className='home-title-font'> aspiring Web Developer</div>
         </div>
     );
 };
