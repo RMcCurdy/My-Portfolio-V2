99d8e6d07258ff616cfcc5843285e739f523f5ca
Robert McCurdy <rwmccurdy@hotmail.com>
2022-01-01 22:44:07 -0800

    Navbar styling

--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -27,21 +27,59 @@ const Navbar = () => {
             }}
             className='navbar-container'>
             <div
+                style={{
+                    borderBottom:
+                        location.pathname === '/'
+                            ? '3px solid black'
+                            : '2px solid black',
+                    paddingBottom: location.pathname === '/' ? '1px' : '0',
+                    fontFamily: location.pathname === '/' ? 'Rubik' : 'Rubik-S',
+                }}
                 className='navbar-titles'
                 onClick={() => handleNavbarClick('')}>
                 Home
             </div>
             <div
+                style={{
+                    borderBottom:
+                        location.pathname === '/skills'
+                            ? '3px solid black'
+                            : '2px solid black',
+                    paddingBottom:
+                        location.pathname === '/skills' ? '1px' : '0',
+                    fontFamily:
+                        location.pathname === '/skills' ? 'Rubik' : 'Rubik-S',
+                }}
                 className='navbar-titles'
                 onClick={() => handleNavbarClick('skills')}>
                 Skills
             </div>
             <div
+                style={{
+                    borderBottom:
+                        location.pathname === '/projects'
+                            ? '3px solid black'
+                            : '2px solid black',
+                    paddingBottom:
+                        location.pathname === '/projects' ? '1px' : '0',
+                    fontFamily:
+                        location.pathname === '/projects' ? 'Rubik' : 'Rubik-S',
+                }}
                 className='navbar-titles'
                 onClick={() => handleNavbarClick('projects')}>
                 Projects
             </div>
             <div
+                style={{
+                    borderBottom:
+                        location.pathname === '/contact'
+                            ? '3px solid black'
+                            : '2px solid black',
+                    paddingBottom:
+                        location.pathname === '/contact' ? '1px' : '0',
+                    fontFamily:
+                        location.pathname === '/contact' ? 'Rubik' : 'Rubik-S',
+                }}
                 className='navbar-titles'
                 onClick={() => handleNavbarClick('contact')}>
                 Contact
