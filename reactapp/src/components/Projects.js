d65204fd8db3b52ca7a482cd35332590eef3a323
Robert McCurdy <rwmccurdy@hotmail.com>
2022-01-03 18:05:21 -0800

    Logo added to Projects

--- reactapp/src/components/Projects.js
+++ reactapp/src/components/Projects.js
@@ -4,6 +4,14 @@ import MyGHPreview from '../images/myGH2.png';
 import DiscordPreview from '../images/discord.png';
 import WaitLessPreview from '../images/WaitLess.png';
 import SkipTheBooksPreview from '../images/SkipTheBooks.png';
+import ReactLogo from '../images/ReactLogo.png';
+import JSLogo from '../images/JSLogo.png';
+import HTMLLogo from '../images/HTMLLogo.png';
+import CSSLogo from '../images/CSSLogo.png';
+import CSharpLogo from '../images/CSharpLogo.png';
+import SQLLogo from '../images/SQLLogo.png';
+import AdobeXDLogo from '../images/AdobeXDLogo.png';
+import BalsamiqLogo from '../images/BalsamiqLogo.png';
 
 const Projects = () => {
     return (
@@ -20,17 +28,61 @@ const Projects = () => {
                         <div className='project-description'>
                             A web application that utilizes NBA Team's and
                             player data to give accurate predictions for the
-                            outcome of games
+                            outcome of games.
                         </div>
-                        <div
-                            className='project-links'
-                            onClick={() =>
-                                window.open(
-                                    'https://github.com/RMcCurdy/Bandwagon',
-                                    '_blank',
-                                )
-                            }>
-                            GitHub
+                        <div className='project-links-and-languages-container'>
+                            <div
+                                className='project-links'
+                                onClick={() =>
+                                    window.open(
+                                        'https://github.com/RMcCurdy/Bandwagon',
+                                        '_blank',
+                                    )
+                                }>
+                                GitHub
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={ReactLogo}
+                                    alt='react'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={JSLogo}
+                                    alt='javascript'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={HTMLLogo}
+                                    alt='html'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={CSSLogo}
+                                    alt='css'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={SQLLogo}
+                                    alt='sql'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={CSharpLogo}
+                                    alt='csharp'
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -42,26 +94,56 @@ const Projects = () => {
                             A web application that implements the GitHub REST
                             API to show detailed user information.
                         </div>
-                        <div className='project-links-container'>
-                            <div
-                                className='project-links'
-                                onClick={() =>
-                                    window.open(
-                                        'https://github.com/RMcCurdy/MyGH',
-                                        '_blank',
-                                    )
-                                }>
-                                GitHub
+                        <div className='project-links-and-languages-container'>
+                            <div className='project-links-container'>
+                                <div
+                                    className='project-links'
+                                    onClick={() =>
+                                        window.open(
+                                            'https://github.com/RMcCurdy/MyGH',
+                                            '_blank',
+                                        )
+                                    }>
+                                    GitHub
+                                </div>
+                                <div
+                                    className='project-links'
+                                    onClick={() =>
+                                        window.open(
+                                            'https://rmccurdy.github.io/MyGH/',
+                                            '_blank',
+                                        )
+                                    }>
+                                    Website
+                                </div>
                             </div>
-                            <div
-                                className='project-links'
-                                onClick={() =>
-                                    window.open(
-                                        'https://rmccurdy.github.io/MyGH/',
-                                        '_blank',
-                                    )
-                                }>
-                                Website
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={ReactLogo}
+                                    alt='react'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={JSLogo}
+                                    alt='javascript'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={HTMLLogo}
+                                    alt='html'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={CSSLogo}
+                                    alt='css'
+                                />
                             </div>
                         </div>
                     </div>
@@ -79,28 +161,58 @@ const Projects = () => {
                         <div className='project-description'>
                             Submission for CalgaryHack2021. A web application
                             that provides students utilities for tracking their
-                            school schedule and ordering takeout
+                            school schedule and ordering takeout.
                         </div>
-                        <div className='project-links-container'>
-                            <div
-                                className='project-links'
-                                onClick={() =>
-                                    window.open(
-                                        'https://github.com/RMcCurdy/CalgaryHacks2021',
-                                        '_blank',
-                                    )
-                                }>
-                                GitHub
+                        <div className='project-links-and-languages-container'>
+                            <div className='project-links-container'>
+                                <div
+                                    className='project-links'
+                                    onClick={() =>
+                                        window.open(
+                                            'https://github.com/RMcCurdy/CalgaryHacks2021',
+                                            '_blank',
+                                        )
+                                    }>
+                                    GitHub
+                                </div>
+                                <div
+                                    className='project-links'
+                                    onClick={() =>
+                                        window.open(
+                                            'https://duan-le.github.io/CalgaryHacks2021/',
+                                            '_blank',
+                                        )
+                                    }>
+                                    Website
+                                </div>
                             </div>
-                            <div
-                                className='project-links'
-                                onClick={() =>
-                                    window.open(
-                                        'https://duan-le.github.io/CalgaryHacks2021/',
-                                        '_blank',
-                                    )
-                                }>
-                                Website
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={ReactLogo}
+                                    alt='react'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={JSLogo}
+                                    alt='javascript'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={HTMLLogo}
+                                    alt='html'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={CSSLogo}
+                                    alt='css'
+                                />
                             </div>
                         </div>
                     </div>
@@ -113,31 +225,48 @@ const Projects = () => {
                     />
                     <div className='project-text-container'>
                         <div className='project-title'>WaitLess</div>
+
                         <div className='project-description'>
                             A mobile application concept to improve the virtual
                             academic advising experience for students by
                             refining a virtual line up system.
                         </div>
-                        <div className='project-links-container'>
-                            <div
-                                className='project-links'
-                                onClick={() =>
-                                    window.open(
-                                        'https://github.com/RMcCurdy/TeamS_Project',
-                                        '_blank',
-                                    )
-                                }>
-                                GitHub
+                        <div className='project-links-and-languages-container'>
+                            <div className='project-links-container'>
+                                <div
+                                    className='project-links'
+                                    onClick={() =>
+                                        window.open(
+                                            'https://github.com/RMcCurdy/TeamS_Project',
+                                            '_blank',
+                                        )
+                                    }>
+                                    GitHub
+                                </div>
+                                <div
+                                    className='project-links'
+                                    onClick={() =>
+                                        window.open(
+                                            'https://edmunds13.github.io/#',
+                                            '_blank',
+                                        )
+                                    }>
+                                    Website
+                                </div>
                             </div>
-                            <div
-                                className='project-links'
-                                onClick={() =>
-                                    window.open(
-                                        'https://edmunds13.github.io/#',
-                                        '_blank',
-                                    )
-                                }>
-                                Website
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={AdobeXDLogo}
+                                    alt='adobexd'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={BalsamiqLogo}
+                                    alt='balsamiq'
+                                />
                             </div>
                         </div>
                     </div>
@@ -156,15 +285,31 @@ const Projects = () => {
                             An AI that can perform a number of useful automated
                             tasks and bot commands on a Discord server.
                         </div>
-                        <div
-                            className='project-links'
-                            onClick={() =>
-                                window.open(
-                                    'https://github.com/RMcCurdy/Discord-Bot',
-                                    '_blank',
-                                )
-                            }>
-                            GitHub
+                        <div className='project-links-and-languages-container'>
+                            <div
+                                className='project-links'
+                                onClick={() =>
+                                    window.open(
+                                        'https://github.com/RMcCurdy/Discord-Bot',
+                                        '_blank',
+                                    )
+                                }>
+                                GitHub
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={JSLogo}
+                                    alt='javascript'
+                                />
+                            </div>
+                            <div className='project-logo-container'>
+                                <img
+                                    className='language-logo'
+                                    src={BalsamiqLogo}
+                                    alt='balsamiq'
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
