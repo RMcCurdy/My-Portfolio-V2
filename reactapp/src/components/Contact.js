2d1253562a68deb5567416d2b4906297f5e5d574
Robert McCurdy <rwmccurdy@hotmail.com>
2022-01-04 01:19:32 -0800

    Finished contact page without mailto

--- reactapp/src/components/Contact.js
+++ reactapp/src/components/Contact.js
@@ -3,8 +3,23 @@ import LinkedInLogo from '../images/LinkedInLogo.png';
 import ResumeLogo from '../images/ResumeLogo.png';
 
 const Contact = () => {
+    // function sendEmail(address) {
+    //     window.location.href = 'mailto:' + address;
+    // }
+
     return (
         <div className='contact-container'>
+            <div className='contact-socials-email-header'>
+                You can get in contact with me by sending an email to:
+            </div>
+            <div
+                // onClick={() => sendEmail('rwmccurdy@hotmail.com')}
+                className='contact-socials-email'>
+                rwmccurdy@hotmail.com
+            </div>
+            <div className='contact-socials-header'>
+                Feel free to view my GitHub, LinkedIn, or Resume below.
+            </div>
             <div className='contact-socials-container'>
                 <img
                     onClick={() =>
@@ -15,11 +30,23 @@ const Contact = () => {
                     alt='github'
                 />
                 <img
+                    onClick={() =>
+                        window.open(
+                            'https://www.linkedin.com/in/robert-mccurdy/',
+                            '_blank',
+                        )
+                    }
                     className='contact-socials'
                     src={LinkedInLogo}
                     alt='linkedin'
                 />
                 <img
+                    onClick={() =>
+                        window.open(
+                            'https://drive.google.com/file/d/1CaDL7z_ibbJcyZ6gRmV6OVGgWmW7kU-r/view?usp=sharing',
+                            '_blank',
+                        )
+                    }
                     className='contact-socials'
                     src={ResumeLogo}
                     alt='resume'
